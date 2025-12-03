0f103943b30116e9860ffc88f1821958c0fd58cc
Peter Emu <emu.kimani@strathmore.edu>
2025-12-03 09:38:44 +0300

    get update and delete method added

--- assistant.py
+++ assistant.py
@@ -17,3 +17,18 @@ def create_user():
 @app.get("/user")
 def get_users():
     return[]
+#get a single user
+@app.get("/user/{user_id}")
+def get_user(user_id):
+    #user=db.query(User).filter(id == user_id).first()
+    return {"id":user_id}
+
+#update a single user
+@app.patch("/user/{user_id}")
+def update_user(user_id):
+    return{}
+
+#delete a single user
+@app.delete("/user/{user_id}")
+def delete_user(user_id):
+    return{}
